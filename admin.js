a4285c9bc9eb610e713fdf18497fe5e44766cbad
853204 <siddharthkumar853204@gmail.com>
2023-02-23 01:53:50 +0530

    day-2

---
+++ admin.js
@@ -0,0 +1,14 @@
+let btn=document.getElementById("btn")
+btn.addEventListener("click",()=>{
+    window.location.href='loginadmin.html'
+})
+// fetch('https://63f5b7b2ab76703b15b0397b.mockapi.io/Products')
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
