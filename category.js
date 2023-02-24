fd1bc5dd06890cdc26aa23bf3eca734457976a99
Shan-Ali4 <shanalicoc@gmail.com>
2023-02-24 21:32:53 +0530

    Shan Day-4

--- category.js
+++ category.js
@@ -1,3 +1,11 @@
+function toggleDropdown(span) {
+  var dropdown = span.nextElementSibling;
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
 
 fetch('https://testapi2-9psr.onrender.com/Products')
   .then(response => response.json())
@@ -14,17 +22,18 @@ fetch('https://testapi2-9psr.onrender.com/Products')
       cartcontainer.innerHTML="";
       data.forEach((ele,ind )=> {
         let  div=document.createElement("div")
-        let image=document.createElement("img")  
+        let image=document.createElement("img")
+        let hr=document.createElement("hr")  
         let he1=document.createElement("h3") 
         let he2=document.createElement("h4") 
         let p1=document.createElement("p")  
         let p2=document.createElement("p")
-        p2.innerText=ele.price;
+        p2.innerText=("₹",ele.price);
         image.src=ele.image;
         he1.innerText=ele.title;
-        he2.innerText=ele.brand;
-        p1.innerText=ele.category;
+        // he2.innerText=ele.brand;
+        // p1.innerText=ele.category;
         cartcontainer.append(div)
-        div.append(image,he1,he2,p1,p2)
+        div.append(imag,hr,p2,he1,he2,p1,)
       });
     }
