ab2363e136801ce119372290c27d4556b630a0d3
Vicky-Sarkar <vickyysarkar@gmail.com>
2023-02-25 21:33:54 +0530

    authentication js codes added

--- login/login.js
+++ login/login.js
@@ -1,15 +1,29 @@
 let form = document.getElementById("form")
-
+let form1 = document.getElementById("form1")
 form.addEventListener("submit", (e) => {
 	e.preventDefault()
 	let userinput = document.getElementById("contact")
-	console.log(userinput.length)
+	console.log(userinput.value.length)
 
 
 	if (userinput.value.length >= 10) {
-		userinput.style.Color = "red"
-	}
 
+		let otpp = '';
+		for (let i = 0; i < 4; i++) {
+			otpp += Math.floor(Math.random() * 10)
+		}
+		alert(otpp)
+	}
 })
 
 
+form1.addEventListener("submit", (e) => {
+	let validation = document.getElementById("verify")
+	e.preventDefault();
+
+	if (validation.value.length >= 4) {
+		//window.location.href =""
+
+		console.log("hi")
+	}
+})
